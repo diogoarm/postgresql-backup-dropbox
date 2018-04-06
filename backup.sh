a14fa7d871d8bffb3afa4fb980a9477df5b391ad
Diogo Monteiro <diogo.arm@gmail.com>
2018-04-06 18:39:21 -0300

    corrige seleção dos nomes das bases

--- backup.sh
+++ backup.sh
@@ -4,14 +4,14 @@ set -eu
 
 export PGPASSWORD=$POSTGRESQL_PASSWORD
 POSTGRESQL_HOST_OPTS="-h $POSTGRESQL_HOST -p $POSTGRESQL_PORT -U $POSTGRESQL_USER"
-databases=$(psql $POSTGRESQL_HOST_OPTS -c "\l")
+databases=$(psql $POSTGRESQL_HOST_OPTS -c "SELECT datname FROM pg_database WHERE datistemplate = false;")
 
 for database in $databases
 do
   if [ "$database" != "information_schema" ]
   then
     echo "Creating backup for $database..."
-    pgdump $POSTGRESQL_HOST_OPTS $POSTGRESQLDUMP_OPTIONS $database \
+    pg_dump $POSTGRESQL_HOST_OPTS $POSTGRESQLDUMP_OPTIONS $database \
       | gzip \
       | curl -X PUT \
              -H "Authorization: Bearer $DROPBOX_ACCESS_TOKEN" \
