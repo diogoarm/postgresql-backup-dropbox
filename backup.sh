5edd92c0915135106705cd730fa2dee4f91fec36
Diogo Monteiro <diogo.arm@gmail.com>
2018-04-07 17:23:35 -0300

    update commnet

--- backup.sh
+++ backup.sh
@@ -5,7 +5,7 @@ set -eu
 export PGPASSWORD=$POSTGRESQL_PASSWORD
 POSTGRESQL_HOST_OPTS="-h $POSTGRESQL_HOST -p $POSTGRESQL_PORT -U $POSTGRESQL_USER"
 
-echo "Creating backup for $database..."
+echo "Creating backup for $DATABASE_NAME..."
 pg_dump $POSTGRESQL_HOST_OPTS $POSTGRESQLDUMP_OPTIONS $DATABASE_NAME \
   | gzip \
   | curl -X PUT \
