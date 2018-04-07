36a304064833789691754ad0a11b8ef59e74c676
Diogo Monteiro <diogo.arm@gmail.com>
2018-04-07 17:40:30 -0300

    update

--- backup.sh
+++ backup.sh
@@ -8,9 +8,10 @@ POSTGRESQL_HOST_OPTS="-h $POSTGRESQL_HOST -p $POSTGRESQL_PORT -U $POSTGRESQL_USE
 echo "Creating backup for $DATABASE_NAME..."
 pg_dump $POSTGRESQL_HOST_OPTS $POSTGRESQLDUMP_OPTIONS $DATABASE_NAME > $DATABASE_NAME.sql \
   | gzip $DATABASE_NAME.sql \
-  | curl -X PUT \
+  | curl -v -X PUT \
           -H "Authorization: Bearer $DROPBOX_ACCESS_TOKEN" \
           -T - \
           --silent \
           --output /dev/null \
           https://content.dropboxapi.com/1/files_put/auto/$DATABASE_NAME.sql.gz
+rm -f $DATABASE_NAME.sql $DATABASE_NAME.sql.gz
